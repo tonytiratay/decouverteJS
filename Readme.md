eb8ea96f0ec99a90ccb842d956343c839124e270
Tony Tiratay <tony.tiratay@gmail.com>
2018-10-31 09:52:56 +0100

    Change everything :)

---
+++ Readme.md
@@ -0,0 +1,44 @@
+# Le javascript, c'est quoi ?
+
+Le javascript est un langage initialement créé pour être executé dans un navigateur.
+
+**Son but original:**
+
+Permettre de programmer des interactions avec le HTML / CSS
+
+> Ex: Je clique sur un bouton pour afficher / masquer une partie de ma page.
+> Ex: Quand je clique sur un menu la page s'anime et descend jusqu'à la bonne rubrique
+> Ex: Quand je reçois un message, sans recharger la page, une notification m'en informe
+
+**Comment ça marche ?**
+
+Le navigateur est un programme, qui lit le code javascript, et est capable de le transformer en instructions machine.
+
+> Dans le cas de Chrome, le moteur de rendu javascript s'appelle V8.
+
+**Ok, pourquoi tu nous dis tout ça ?**
+
+Pour vous faire comprendre que le contexte dans lequel vous aller executer le javascript détermine ce qui est possible ou non. Dans un navigateur par exemple, impossible de parcourir soi-même le contenu du disque dur de la machine. Mais V8 à changé la donne...
+
+> Aujourd'hui V8 permet à d'autres choses que le navigateur de comprendre le js
+> Comme par exemple, un serveur, ou mon ordinateur
+
+**Ok... Mais pour quoi faire ?**
+
+Pour permettre de faire avec un seul langage ce qui avant en nécessitait plusieurs
+
+* Faire des applications Web
+
+* Faire des applications mobile / tablette
+
+* Faire des logiciels
+
+* Faire des programmes en ligne de commande...
+
+**Ah, mais alors c'est super facile !**
+
+Mmmmm, oui... Mais non :) 
+
+L'univers de javascript et de ses bibliothèques est énorme, et pour beaucoup ces technologies sont jeunes, donc tout ça fait que tout n'est pas forcément toujours stable...
+
+Et les concepts qu'ils faut comprendre pour faire de ces autres langages s'appliquent aussi à javascript !
--- demo.js
+++ demo.js
@@ -1,92 +1,28 @@
-// Afficher du contenu dans la console
-console.log('Hello World depuis le fichier index.js!');
+// Je stocke dans des variable les éléments HTML
+// Je trouve ces éléments HTML grâce à leur Id.
+// Ex. zoneDeSaisie, contenuHTML
 
-// Les variables peuvent contenir tous type de contenu (data types).
-// Par exemple:
-// String (chaine de caractères)
-var monPrenom = 'Tony';
-var jolieSeparation = '---------------------------------------';
+var zoneDeSaisie = document.getElementById('input');
+var contenuHTML = document.getElementById('contenuHTML');
 
-console.log(jolieSeparation);
+// Je rempli zoneDeSaisie avec le contenu
+// de la div "contenuHTML"
 
-// Number
-var monAge = 33;
-var monAgeMental = 18;
+zoneDeSaisie.value = contenuHTML.innerHTML;
 
-// Boolean (vrai ou faux)
-var jeSuisDrole = false;
+// Je crée une fonction qui met à jour
+// le contenu de la div "contenuHTML"
+// en y insérant le contenu de l'input "zoneDeSaisie"
 
-// Array (tableau)
-var mesInterets = ['code', 'javascript', 'video', 'manger'];
-
-// Object (ensemble de clé / valeur)
-var superHeroine = {
-  prenom: 'Ada', // Key: prenom, Value: Ada
-  nom: 'Lovelace',
-  metier: 'Développeuse',
+var mettreAJourLaDiv = function () {
+  contenuHTML.innerHTML = zoneDeSaisie.value;
 };
 
-// function
+// J'écoute sur la zoneDeSaisie l'événement "onkeyup"
+// Cet événement est déclenché dés qu'un
+// utilisateur relache une touche.
+// J'assigne la fonction mettreAJourLaDiv comme
+// fonction à executer
+// lorsque l'événement est détecté
 
-// Fonction sans paramètres
-
-var direBonjour = function () {
-  console.log('Salut !');
-};
-
-// Fonction avec paramètres
-
-var sePresenter = function (prenom, age, ageMental, interets) {
-
-  // Une fonction peut prendre des paramètres (entre les parenthèses ci dessus)
-  // afin d'en faire, ou non, quelque chose
-  console.log('Salut, je suis ' + prenom + '!');
-
-  // exemple: "Salut, je suis Tony!"
-  console.log("J'ai " + age + ' ans dans mon corps,');
-
-  // exemple: "J'ai 33 ans dans mon corps,""
-  console.log('mais ' + ageMental + ' dans ma tête...');
-
-  // exemple: "mais 18 dans ma tête...""
-  console.log('Mes intérêts sont au nombre de ' + interets.length);
-
-  // exemple: "Mes intérêts sont au nombre de 2"
-};
-
-// Pour utiliser une fonction, il suffit de l'appeler avec
-// une paire de parenthèses à la fin
-
-direBonjour();
-
-// Pour l'appeler en lui donnant des paramètres, même chose
-// en lui passant les paramètres à l'intérieur des paramètres
-
-sePresenter('Michel', 41, 87, ['Raler', 'Faire des blagues douteuses']);
-
-console.log(jolieSeparation);
-
-// Les paramètres peuvent aussi être des variables
-
-sePresenter(monPrenom, monAge, monAgeMental, mesInterets);
-
-console.log(jolieSeparation);
-
-// Les objets peuvent contenir dans leurs propriétés la même chose que les variables
-// Pour accéder au contenu d'une propriété de l'objet, depuis l'objet, on utilise "this"
-// Dans ce contexte, "this" représente l'objet lui même
-
-var monChien = {
-  prenom: 'Maia',
-  age: 18,
-  uniteAge: 'mois',
-  direBonjour: function () { // Une fonction dans un objet est appelée "method"
-    console.log('Wouaf, je suis ' + this.prenom);
-    console.log("et j'ai " + this.age + ' ' + this.uniteAge + '!');
-  },
-};
-
-// Pour accéder à une propriété d'objet
-
-console.log(monChien.prenom); // Affiche "Maia"
-monChien.direBonjour(); // Appel la méthode (fonction) direBonjour de l'objet
+zoneDeSaisie.onkeyup = mettreAJourLaDiv;
